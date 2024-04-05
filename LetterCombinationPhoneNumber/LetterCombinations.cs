5ed2f7093f490606ac99487d8c02db0c54bafab6
Дмитрий Терещенко <dmitriy.tereshchenko1989@gmail.com>
2024-04-05 15:04:51 +0300

    Add project files.

---
+++ LetterCombinationPhoneNumber/LetterCombinations.cs
@@ -0,0 +1,38 @@
+﻿namespace LetterCombinationPhoneNumber
+{
+	public class LetterCombinations
+	{
+		private readonly string[] buttons = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
+		public List<string> Combinations(string digits)
+		{
+			if (string.IsNullOrEmpty(digits))
+			{
+				return [];
+			}
+			List<string> combinations = [];
+			Queue<string> queueCombinations = [];
+			queueCombinations.Enqueue("");
+			int dIdx = 0;
+			while (queueCombinations.Count > 0)
+			{
+				for (int count = queueCombinations.Count; count > 0; --count)
+				{
+					string curCombination = queueCombinations.Dequeue();
+					string letters = buttons[digits[dIdx] - '0'];
+					foreach (char letter in letters)
+					{
+						string newCombination = curCombination + letter;
+						if (dIdx == digits.Length - 1)
+						{
+							combinations.Add(newCombination);
+							continue;
+						}
+						queueCombinations.Enqueue(newCombination);
+					}
+				}
+				++dIdx;
+			}
+			return combinations;
+		}
+	}
+}
---
+++ LetterCombinationUnitTests/LetterCombinationTests.cs
@@ -0,0 +1,46 @@
+﻿using LetterCombinationPhoneNumber;
+
+namespace LetterCombinationUnitTests
+{
+	public class LetterCombinationTests
+	{
+		private readonly LetterCombinations combinations = new();
+
+		private bool IsSame(List<string> answer, List<string> curResult)
+		{
+			if (answer.Count != curResult.Count)
+			{
+				return false;
+			}
+			for (int i = 0; i < answer.Count; ++i)
+			{
+				if (answer[i] != curResult[i])
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		[Test]
+		public void Letter_Combination_With_String1()
+		{
+			List<string> result = combinations.Combinations("23");
+			Assert.That(IsSame(["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"], result), Is.EqualTo(true));
+		}
+
+		[Test]
+		public void Letter_Combination_Test_With_String2()
+		{
+			List<string> result = combinations.Combinations("2");
+			Assert.That(IsSame(["a", "b", "c"], result), Is.EqualTo(true));
+		}
+
+		[Test]
+		public void Letter_Combination_With_Empty_String()
+		{
+			List<string> result = combinations.Combinations("");
+			Assert.That(result, Is.Empty);
+		}		
+	}
+}
