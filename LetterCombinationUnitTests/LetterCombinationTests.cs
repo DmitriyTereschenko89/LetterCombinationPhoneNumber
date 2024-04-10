ade28aa2b5bcf6ae03de461e4d1186fb457dab0d
Дмитрий Терещенко <dmitriy.tereshchenko1989@gmail.com>
2024-04-10 23:17:26 +0300

    change testcases

--- LetterCombinationUnitTests/LetterCombinationTests.cs
+++ LetterCombinationUnitTests/LetterCombinationTests.cs
@@ -5,42 +5,14 @@ namespace LetterCombinationUnitTests
 	public class LetterCombinationTests
 	{
 		private readonly LetterCombinations combinations = new();
-
-		private bool IsSame(List<string> answer, List<string> curResult)
+		
+		[TestCase("23", "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf")]
+		[TestCase("2", "a", "b", "c")]
+		[TestCase("")]
+		public void Letter_Combination_With_String1(string digits, params string[] expectedResult)
 		{
-			if (answer.Count != curResult.Count)
-			{
-				return false;
-			}
-			for (int i = 0; i < answer.Count; ++i)
-			{
-				if (answer[i] != curResult[i])
-				{
-					return false;
-				}
-			}
-			return true;
+			List<string> result = combinations.Combinations(digits);
+			Assert.That(result.SequenceEqual(expectedResult), Is.EqualTo(true));
 		}
-
-		[Test]
-		public void Letter_Combination_With_String1()
-		{
-			List<string> result = combinations.Combinations("23");
-			Assert.That(IsSame(["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"], result), Is.EqualTo(true));
-		}
-
-		[Test]
-		public void Letter_Combination_Test_With_String2()
-		{
-			List<string> result = combinations.Combinations("2");
-			Assert.That(IsSame(["a", "b", "c"], result), Is.EqualTo(true));
-		}
-
-		[Test]
-		public void Letter_Combination_With_Empty_String()
-		{
-			List<string> result = combinations.Combinations("");
-			Assert.That(result, Is.Empty);
-		}		
 	}
 }
